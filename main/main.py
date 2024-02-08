53ee4a77287047f4e6a0e799c2d004584ad6dd3d
Nikandrov Dmitry <dnikandrov@yandex.ru>
2024-02-08 22:35:42 +0300

    2A

--- main/main.py
+++ main/main.py
@@ -5,3 +5,8 @@ operation_all = load_function()
 operation_filted = filter_and_mask_account(operation_all)
 operation_sorted = sorted_list(operation_filted)
 operation_data_transform = data_transform(operation_sorted)
+
+for operation in operation_data_transform:
+  print (f'''{operation['date']} {operation['description']}
+{operation['from']} -> {operation['to']}
+{operation['amount']} {operation['name']}\n''')
